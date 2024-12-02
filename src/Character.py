417e57b146bd9fb36424ff8d9973e823a263d06e
Sofie Caballes <scaballes@binghamton.edu>
2024-12-01 20:36:21 -0500

    update

---
+++ src/Character.py
@@ -0,0 +1,14 @@
+class Character: 
+
+    def __init__(self, x, y, img_file):
+        """ initializes character object 
+
+        Args:
+            x (int): position 
+            y (int): position 
+            img_file (str): visual of character 
+        """
+        self = self.__init__
+        x = self.x
+        y = self.y 
+        img_file = self.img_file
