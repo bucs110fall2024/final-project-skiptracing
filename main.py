d9450444da347428639d2b89d76363d584de1251
Sofie Caballes <scaballes@binghamton.edu>
2024-12-03 23:44:21 -0500

    colors and surfaces

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ test_font = pygame.font.Font('assets/Font/Pixeltype.ttf', 50)
 # Create surfaces 
 sky_surface = pygame.image.load('assets/sky.png').convert()
 ground_surface = pygame.image.load('assets/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black').convert()
+text_surface = test_font.render('My game', False, 'Black').convert() # can change to rgb tuple :) 
+text_rect = text_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('assets/Snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (700, 300))
@@ -36,13 +37,16 @@ while True:
 
     screen.blit(sky_surface, (0,0)) # Blot image transfer 
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (300,50))
+    pygame.draw.rect(screen, 'Pink', text_rect) # '#c0e8dc' hexadecimal color :) 
+    pygame.draw.rect(screen, 'Pink', text_rect, 10) # draw rectangle shape 
+    screen.blit(text_surface, text_rect)
 
     snail_rect.x -= 4 
     if snail_rect.right <= 0: 
         snail_rect.left = 800 
     screen.blit(snail_surface, snail_rect)
-    player_rect.left += 1 # dont move surf, move rect that contains surf
+    
+    #player_rect.left += 1 # dont move surf, move rect that contains surf
     screen.blit(player_surface, player_rect) # placing at rect 
     
     pygame.display.update()
