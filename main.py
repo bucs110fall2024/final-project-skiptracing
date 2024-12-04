2f966a1473263c452099566fa9d1260a168079c7
Sofie Caballes <scaballes@binghamton.edu>
2024-12-04 00:24:33 -0500

    scpre timer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame 
 import sys 
 
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time # time in ms - start time so time doesn't go forever 
+    score_surface = test_font.render(f"{current_time}", False, (64, 64, 64))
+    score_rect = score_surface.get_rect(center = (400, 50))
+    screen.blit(score_surface, score_rect)
+    
 # Initialize pygame and variables 
 pygame.init()
 screenWidth = 800 
@@ -12,12 +18,14 @@ pygame.display.set_icon(icon)
 clock = pygame.time.Clock() # Clock object for FPS
 test_font = pygame.font.Font('assets/Font/Pixeltype.ttf', 50) 
 game_active = True 
+start_time = 0 
 
 # Create surfaces 
 sky_surface = pygame.image.load('assets/sky.png').convert()
 ground_surface = pygame.image.load('assets/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black').convert() # can change to rgb tuple :) 
-text_rect = text_surface.get_rect(center = (400, 50))
+# text_surface = test_font.render('My game', False, 'Black').convert() # can change to rgb tuple :) 
+# text_rect = text_surface.get_rect(center = (400, 50))
+
 
 snail_surface = pygame.image.load('assets/Snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (700, 300))
@@ -52,14 +60,16 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
 
     if game_active == True: 
         # Draw Background 
         screen.blit(sky_surface, (0,0)) # Blot image transfer 
         screen.blit(ground_surface, (0,300))
-        pygame.draw.rect(screen, 'Pink', text_rect) # '#c0e8dc' hexadecimal color :) 
-        pygame.draw.rect(screen, 'Pink', text_rect, 10) # draw rectangle shape 
-        screen.blit(text_surface, text_rect)
+        # pygame.draw.rect(screen, 'Pink', text_rect) # '#c0e8dc' hexadecimal color :) 
+        # pygame.draw.rect(screen, 'Pink', text_rect, 10) # draw rectangle shape 
+        # screen.blit(text_surface, text_rect)
+        display_score()
 
         # Snail Movement 
         snail_rect.x -= 4 # move surf that contains rect 
